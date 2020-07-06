257db77033fc1a5c81e9d1d073776d4a9c0182c5
Grgur Grisogono <grgur@grgur.com>
2020-07-06 11:50:08 +0200

    component testing

--- src/components/header.spec.tsx
+++ src/components/header.spec.tsx
@@ -1,11 +1,16 @@
 import React from "react"
 import renderer from "react-test-renderer"
+import { render } from "@testing-library/react"
 import Header from "./header"
+
 describe("Header", () => {
   it("renders correctly", () => {
-    const tree = renderer
-      .create(<Header siteTitle="Default Starter" />)
-      .toJSON()
+    const tree = renderer.create(<Header />).toJSON()
     expect(tree).toMatchSnapshot()
+  })
+
+  it("renders the title", () => {
+    const { getByTestId } = render(<Header />)
+    expect(getByTestId("title")).toHaveTextContent("GrgurdeGrisogono")
   })
 })
